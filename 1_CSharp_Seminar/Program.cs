5743056433f495cf0040561a9db8ff5b03ef660e
Marina <linamamarinina@gmail.com>
2022-07-09 11:51:13 +0300

    Task n.1

---
+++ 1_CSharp_Seminar/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+// 1. Программа на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+int num1, num2; //quad убрали
+
+Console.Write("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());  //переводим число из "23" в 23
+Console.Write("Input second integer number: "); 
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2 * num2)   //скобки убрали, т.к. одно действие
+    Console.WriteLine("Yes");  // отступы нужны. Строчка - в подчинении.
+else 
+    Console.WriteLine("No");
+*/
+
+/*
+// 1. Задача через переменную string result. Верхний вариант более оптимизирован/быстрый.
+ int num1, num2; 
+string result;
+Console.Write("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());  //переводим число из "23" в 23
+Console.Write("Input second integer number: "); 
+num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 == num2 * num2)   //скобки убрали, т.к. одно действие
+    result = "Yes";  // отступы нужны: трочка - в подчинении.
+else 
+    result = "No";
+    Console.WriteLine(result);*/
