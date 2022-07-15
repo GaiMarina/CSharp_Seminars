fbeb4227ade4bc9481813550bc9cd28c978284b7
Marina <linamamarinina@gmail.com>
2022-07-15 15:02:21 +0300

    3 Seminar

--- 3_CSharp_Seminar/Program.cs
+++ 3_CSharp_Seminar/Program.cs
@@ -1,49 +1,52 @@
 ﻿
 // Программа по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти 
 // (x и y).
-
-void ShowDiapason(int quart)
+/*
+void ShowRange(int quarter)
 {
-    if(quart == 1)Console.WriteLina("x > 0, y > 0");
-    if(quart == 2)Console.WriteLina("x > 0, y > 0");
-    if(quart == 3)Console.WriteLina("x > 0, y > 0");
-    if(quart == 4)Console.WriteLina("x > 0, y > 0");
+    if(quarter == 1)Console.WriteLine("x > 0, y > 0");
+    if(quarter == 2)Console.WriteLine("x < 0, y > 0");
+    if(quarter == 3)Console.WriteLine("x < 0, y < 0");
+    if(quarter == 4)Console.WriteLine("x > 0, y < 0");
 }
-
+*/
+/*
 // Задали диапазон.
 
-void ShowDiapason(int quart)
+void ShowRange(int quarter)
 {
-    if(quart >= 1 && quart <= 4)
+    if(quarter >= 1 && quarter <= 4)
     {
-    if(quart == 1)Console.WriteLine("x > 0, y > 0");
-    if(quart == 2)Console.WriteLine("x > 0, y > 0");
-    if(quart == 3)Console.WriteLine("x > 0, y > 0");
-    if(quart == 4)Console.WriteLine("x > 0, y > 0");
+    if(quarter == 1)Console.WriteLine("x > 0, y > 0");
+    if(quarter == 2)Console.WriteLine("x < 0, y > 0");
+    if(quarter == 3)Console.WriteLine("x < 0, y < 0");
+    if(quarter == 4)Console.WriteLine("x > 0, y < 0");
     }
     else Console.WriteLine("Uncorrect input");
 }
+*/
 
-// Вставили проверку. Надо дописать.
+// Вставили проверку. 
 
-void ShowDiapason(int quart)
+void ShowRange(int quarter)
 {
-    while(quart < 1 || quart > 4)
+    while(quarter < 1 || quarter > 4)
     {
         Console.Write("Uncorrect input! Try again here: ");
-        quart = ConvertюToInt32(Console.ReadLine());
+        quarter = Convert.ToInt32(Console.ReadLine());
     }
-    if(quart == 1)Console.WriteLine("x > 0, y > 0");
-    if(quart == 2)Console.WriteLine("x > 0, y > 0");
-    if(quart == 3)Console.WriteLine("x > 0, y > 0");
-    if(quart == 4)Console.WriteLine("x > 0, y > 0");
+    if(quarter == 1)Console.WriteLine("x > 0, y > 0");
+    if(quarter == 2)Console.WriteLine("x < 0, y > 0");
+    if(quarter == 3)Console.WriteLine("x < 0, y < 0");
+    if(quarter == 4)Console.WriteLine("x > 0, y < 0");
 }
 
-Console.Write("Input number of quart: ");
-int quartNum = Convert.ToInt(Console.ReadLine());
+Console.Write("Input number of quarter: ");
+int quarterNum = Convert.ToInt32(Console.ReadLine());
 
-ShowDiapason(quartNum);
+ShowRange(quarterNum);
 
+/*
 // 2. Программа принимает на вход координаты точки (X и Y), причем X != 0 и Y != 0 и 
 // выдает номер четверти плоскости, в которой находится эта точка.
 
@@ -52,45 +55,45 @@ int Number(int x, int y)
     int result = 0;
     
     if(x > 0 && y > 0) result = 1;
-    if(x > 0 && y > 0) result = 2;
-    if(x > 0 && y > 0) result = 3;
-    if(x > 0 && y > 0) result = 4;
+    if(x < 0 && y > 0) result = 2;
+    if(x < 0 && y < 0) result = 3;
+    if(x > 0 && y < 0) result = 4;
     return result;
 }
 
-Console.Write("Введите координаты X: ");
-int x = Convert.Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координаты Y: ");
-int y = Convert.Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение координаты X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение координаты Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
 int number;
 if (x != 0 && y != 0)
 {
     number = Number(x, y);
-    Console.Write($"Точка относится к {number} четверти");
+    Console.WriteLine($"Точка относится к {number} четверти");
 }
 else Console.WriteLine("Точка находится на оси координат");
+*/
+/*
+// Задача 3. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 
-
-// 3 Программа принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
-
-double Length(int xa, double ya, double xb, double yb);
+double Length(double xa, double ya, double xb, double yb)
 {
-    double length = Math.Sqrt((xb - xa)*(xb - xa) * (yb - ya));
+    double length = Math.Sqrt((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya));
     return length;
 }
+Console.Write("Введите значение координаты Х точки А: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение координаты Y точки А: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение координаты Х точки В: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение координаты Y точки В: ");
+int yb = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите координаты X точки A: ");
-int xa = Convert.Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координаты Y точки A: ");
-int ya = Convert.Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координаты X точки B: ");
-int xb = Convert.Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координаты Y точки B: ");
-int yb = Convert.Convert.ToInt32(Console.ReadLine());
-
-double length = Length(xa, ya, xb, yb);
-Console.Write($"Расстояние между точками A и B равно {length}");
-
+double result = Length(xa, ya, xb, yb);
+Console.WriteLine($"Расточние между точками А и В равно {result}");
+*/
+/*
 // 4. Программа принимает на вход число (N) и выдает последовательность квадратов чисел от 1 до N.
 
 void Numbers(double num)
@@ -107,4 +110,5 @@ double check;
 Console.Write("Введите положительное число: ");
 check = Convert.ToDouble(Console.ReadLine());
 if(check < 0) check = check * (-1);
-Numbers(number);
+Numbers(check);
+*/
