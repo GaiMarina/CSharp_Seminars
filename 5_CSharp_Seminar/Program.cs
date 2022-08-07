818bb220632a128ea9a38369b0b9d3ccf2e02572
Marina <linamamarinina@gmail.com>
2022-08-07 11:04:16 +0300

    Количество элементов массива из заданного отрезка

--- 5_CSharp_Seminar/Program.cs
+++ 5_CSharp_Seminar/Program.cs
@@ -59,7 +59,7 @@ ShowArray(Array);
 Console.WriteLine("The sum of positive elements is " + SumOfPositive(Array));
 */
 //===================================
-
+/*
 // 3.  Написать программу: замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот. 
 // [-4, -8, 8, 2] -> [4, 8, -8, -2] 
 
@@ -82,13 +82,62 @@ ShowArray(NewArray);
 ChangeSign(NewArray);
 ShowArray(NewArray);
 
-
+*///=======================================
+/*
 // 4. Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве. 
-// 3, массив [6, 7, 19, 345, 3] -> да 
-// 4, массив [6, 7, 19, 345, 3] -> нет 
 
+bool FindNumber(int[] array, int num)
+{
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] == num) return true;
+    return false;
+}
+
+Console.Write("Input the size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the min value of an element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the max value of an element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input an integer number to find ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] NewArray = CreateRandomArray(size, min, max);
+ShowArray(NewArray);
+Console.WriteLine(FindNumber(NewArray, number));
+*/
+//=====================
 
 // 5. Задайте одномерный массив из 12 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+
+int[] CreateArrayOfSize(int size)
+{
+    int[] NewArray = new int[size];
+
+    for(int i = 0; i < size; i++)
+    NewArray[i] = new Random().Next();
+    return NewArray;
+}
+
+int CountTheQuantity(int[] array)
+{   
+    int count = 0;
+    
+    for(int i = 0; i < array.Length; i++) 
+
+    if(array[i] >= 10 & array[i] <= 99)
+    count++;
+
+    return count;
+}
+
+int[] NewArray = CreateRandomArray(12,-55, 10);
+
+//int[] NewArray = CreateArrayOfSize(12);
+
+ShowArray(NewArray);
+
+Console.WriteLine($"The number of array elements from the segment [10,99] is {CountTheQuantity(NewArray)}");
 
 // 6. Найти произведение пар чисел в одномерном массиве. Парой считается первый и последний элемент, второй и предпоследний и т.д. Результат записать в новом массиве.
 
