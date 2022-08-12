d906daae86c85a620477a273c68d7395731beb43
Marina <linamamarinina@gmail.com>
2022-08-12 12:39:07 +0300

    Сумма элементов главной диагонали

--- 7_CSharp_Seminar/Program.cs
+++ 7_CSharp_Seminar/Program.cs
@@ -61,7 +61,7 @@ int[,] ArrayCreated = ChangeArrayAsFormula(r, col);
 Show2dArray(ArrayCreated);
 */
 //========================================
-
+/*
  // Задать двумерный массив. Найти элементы, у которых оба индекса чётные, 
  // и замените эти элементы на их квадраты. 
 
@@ -94,13 +94,13 @@ Show2dArray(newArray);
 Console.WriteLine();
 int[,] ArrayCreated = ChangeEvenIndices(newArray);
 Show2dArray(ArrayCreated);
-
+*/
 //=========================================
-/*
-//Задать двумерный массив. 
-//Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д. 
 
-int Sum(int[,] array)
+//Задать двумерный массив. 
+//Найти сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д. 
+
+int SumForMainDiagonal(int[,] array)
  {  
     int sum = 0;
 
@@ -117,7 +117,7 @@ int Sum(int[,] array)
     return sum;
  }
 
- int Sum2(int[,] array)
+ int SumForMainDiagonal2(int[,] array)
  {  
     int sum = 0;
 
@@ -129,4 +129,21 @@ int Sum(int[,] array)
     return sum;
  }
 
-*/
+Console.Write("Input the number of rows ");
+int r = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the number of columns ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the min value of the elements ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the max value of the elements ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] newArray = CreateRandom2dArray(r, col, min, max);
+Show2dArray(newArray);
+Console.WriteLine();
+
+int FinalSum = SumForMainDiagonal(newArray);
+Console.WriteLine(FinalSum);
